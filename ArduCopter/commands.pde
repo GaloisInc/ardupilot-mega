GaloisInc/ardupilot-mega

commit 28f2eb6b9d85f62f02002b2d75250c17841b017f

ACM: removed some Location functions which are now in AP_Math

ArduCopter/navigation.pde
@@ -420,36 +420,3 @@ static int32_t wrap_180(int32_t error)
 	return error;
 }
 
-/*
-//static int32_t get_altitude_above_home(void)
-{
-	// This is the altitude above the home location
-	// The GPS gives us altitude at Sea Level
-	// if you slope soar, you should see a negative number sometimes
-	// -------------------------------------------------------------
-	return current_loc.alt - home.alt;
-}
-*/
-
-// distance is returned in cm
-static int32_t get_distance(struct Location *loc1, struct Location *loc2)
-{
-	float dlat 		= (float)(loc2->lat - loc1->lat);
-	float dlong		= ((float)(loc2->lng - loc1->lng)) * scaleLongDown;
-	dlong			= sqrt(sq(dlat) + sq(dlong)) * 1.113195;
-	return			dlong;
-}
-/*
-//static int32_t get_alt_distance(struct Location *loc1, struct Location *loc2)
-{
-	return abs(loc1->alt - loc2->alt);
-}
-*/
-static int32_t get_bearing(struct Location *loc1, struct Location *loc2)
-{
-	int32_t off_x = loc2->lng - loc1->lng;
-	int32_t off_y = (loc2->lat - loc1->lat) * scaleLongUp;
-	int32_t bearing =	9000 + atan2(-off_y, off_x) * 5729.57795;
-	if (bearing < 0) bearing += 36000;
-	return bearing;
-}

commit 784a403fd2301d4df039526afed31686e17bd55e

ACM: use get_distance_cm() not get_distance()
this fixes a bug introduced in 28f2eb6b9

ArduCopter/commands.pde
@@ -137,7 +137,7 @@ static void set_next_WP(struct Location *wp)
 	if (next_WP.lat == 0 || command_nav_index <= 1){
 		prev_WP = current_loc;
 	}else{
-		if (get_distance(&current_loc, &next_WP) < 500)
+		if (get_distance_cm(&current_loc, &next_WP) < 500)
 			prev_WP = next_WP;
 		else
 			prev_WP = current_loc;
@@ -165,7 +165,7 @@ static void set_next_WP(struct Location *wp)
 
 	// this is handy for the groundstation
 	// -----------------------------------
-	wp_distance 		= get_distance(&current_loc, &next_WP);
+	wp_distance 		= get_distance_cm(&current_loc, &next_WP);
 	target_bearing 		= get_bearing(&prev_WP, &next_WP);
 
 	// calc the location error:

ArduCopter/navigation.pde
@@ -7,8 +7,8 @@ static void navigate()
 {
 	// waypoint distance from plane in cm
 	// ---------------------------------------
-	wp_distance 	= get_distance(&current_loc, &next_WP);
-	home_distance 	= get_distance(&current_loc, &home);
+	wp_distance 	= get_distance_cm(&current_loc, &next_WP);
+	home_distance 	= get_distance_cm(&current_loc, &home);
 
 	// target_bearing is where we should be heading
 	// --------------------------------------------